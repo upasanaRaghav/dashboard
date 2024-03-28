ecb8730ccb2ac5eea50a7e3109d56c490e63dc00
Upasana Raghav <raghavupasana44@gmail.com>
2024-03-28 11:25:52 +0530

    changes

---
+++ src/pages/dashboard/premium.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Box, Button, Typography } from '@mui/material';
+
+const PremiumStats = () => {
+  return (
+    <Box bgcolor="black" p={2} borderRadius={5} color="white" mb={4} display="flex" alignItems="center">
+      <div style={{ marginRight: 'auto' }}>
+        <Typography variant="h6" fontWeight="bold" color="white">
+          Unlock premium stats
+        </Typography>
+        <Typography variant="body1">
+          Get up to 10TB of storage for a limited time
+        </Typography>
+      </div>
+      {/* <Button variant="contained" color="primary" endIcon={<BoltIcon />}> */}
+      <Button variant="contained" color="primary" >
+
+        Upgrade
+      </Button>
+    </Box>
+  );
+};
+
+export default PremiumStats;
